c30e1119afe6449d803d832070aded6bbb548493
Carlos Diaz <carlos.cedp@gmail.com>
2022-01-17 09:54:38 -0400

    page 467

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Filters.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace Filters.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        [CustomAuth(false)]
         public string Index()
         {
             return "This is the Index action on the Home controller";
